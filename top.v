cd0d7e1b730971b22d00b3c0d319da459b6c5d30
freemin7 <25376460+freemin7@users.noreply.github.com>
2023-12-24 22:09:49 +0100

    XMAS pattern for FOMU

--- top.v
+++ top.v
@@ -1,13 +1,20 @@
-module top(input clk25, output reg led);
-
+module top(input clki, output reg rgb0, output reg rgb1, output reg rgb2);
+    reg [0:0] xmas = 0;
     reg [23:0] count = 0;
-    always @(posedge clk25) begin
+    always @(posedge clki) begin
         if (count == 24'd12500000)
             count <= 0;
         else
             count <= count + 1;
         if (count == 0)
-            led <= ~led;
+            if (xmas == 0)
+                rgb0 <= 0;
+            else
+                rgb0 <= 1;
+            
+            rgb1 <= ~rgb0;
+            xmas <= ~xmas;
+            
     end
 
 endmodule
